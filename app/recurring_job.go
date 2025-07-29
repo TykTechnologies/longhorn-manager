b88d36c68d9f2604d9c37f586f1a8e97df5af659
gothka <01jackofalltrades@gmail.com>
2025-07-29 12:53:09 -0300

    fix: revert some of the timeout changes

--- app/recurring_job.go
+++ app/recurring_job.go
@@ -43,8 +43,8 @@ const (
 	WaitInterval              = 5 * time.Second
 	DetachingWaitInterval     = 10 * time.Second
 	VolumeAttachTimeout       = 300  // 5 minutes
-	BackupProcessStartTimeout = 1200 // 20 minutes
-	SnapshotReadyTimeout      = 1200 // 20 minutes
+	BackupProcessStartTimeout = 300 // 6 minutes
+	SnapshotReadyTimeout      = 600 // 10 minutes
 )
 
 type Job struct {
